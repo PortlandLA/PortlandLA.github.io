e81f72ae62d4d3b1aaf0c24ccd2e8d7d3f4adef0
PortlandLA <tmjack@mail.ru>
2017-05-10 16:56:30 +0300

    Add files via upload

--- homework 20/js/index.js
+++ homework 20/js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'Q37gKBggLEg', start: 3 };
+	var options = { videoId: 'gQ-3nBbLRpw', start: 3 };
 	$('.wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
